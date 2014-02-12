3aefed774417c7c19dcf4a7d0a1d73aa24251e32
Dmitri Zagidulin <dzagidulin@gmail.com>
2014-02-11 23:18:40 -0500

    Tweak formatting

--- test/unit/associations_has_one_test.rb
+++ test/unit/associations_has_one_test.rb
@@ -20,35 +20,33 @@
 
 require 'test_helper'
 
-describe "Riagent::Document Associations" do
-  context "has_one association" do
-    it "adds a <target>_key attribute" do
-      # a User model includes a 'has_one :address_book' association
-      User.attribute_set[:address_book_key].wont_be_nil
-      user = User.new
-      user.must_respond_to :address_book_key
-    end
-    
-    it "adds getter and setter methods for the target" do
-      user = User.new
-      user.must_respond_to :address_book
-      user.must_respond_to :address_book=
-    end
-    
-    it "assigning a target object sets corresponding <target>_key and <target> attribute values" do
-      user = User.new
-      address_book = AddressBook.new
-      address_book.key = 'test-book-123'
-      
-      user.address_book = address_book
-      
-      user.address_book_key.must_equal 'test-book-123'
-      
-      user.address_book.must_be_kind_of AddressBook
-    end
-    
-    it "adds a build_<target> method" do
-      
-    end
+describe "Riagent::Document has_one association" do
+  it "adds a <target>_key attribute" do
+    # a User model includes a 'has_one :address_book' association
+    User.attribute_set[:address_book_key].wont_be_nil
+    user = User.new
+    user.must_respond_to :address_book_key
+  end
+
+  it "adds getter and setter methods for the target" do
+    user = User.new
+    user.must_respond_to :address_book
+    user.must_respond_to :address_book=
+  end
+
+  it "assigning a target object sets corresponding <target>_key and <target> attribute values" do
+    user = User.new
+    address_book = AddressBook.new
+    address_book.key = 'test-book-123'
+
+    user.address_book = address_book
+
+    user.address_book_key.must_equal 'test-book-123'
+
+    user.address_book.must_be_kind_of AddressBook
+  end
+
+  it "adds a build_<target> method" do
+
   end
 end
