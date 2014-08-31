538ccb1f1680fd9337c31067caa1bcfc69f13eda
Dmitri Zagidulin <dzagidulin@gmail.com>
2014-08-30 20:23:01 -0400

    RiakDTSetStrategy collections store key lists in Set objects

--- lib/riagent/persistence/riak_dt_set_strategy.rb
+++ lib/riagent/persistence/riak_dt_set_strategy.rb
@@ -24,11 +24,35 @@ require "riagent/persistence/riak_kv_strategy"
 module Riagent
   module Persistence
     class RiakDTSetStrategy < RiakKVStrategy
+      attr_writer :key_list_set
+      
       # Return all the documents in the collection.
       # Uses Riak 2.0 CRDT Set Data type to keep track of collection key list
       # @param [Integer] results_limit Number of results returned (currently ignored)
       # @return [Array<Riagent::ActiveDocument>] List of ActiveDocument instances
       def all(results_limit)
+        keys = self.all_keys
+        # TODO: Trim keys to results_limit
+        all_docs_hash = self.bucket.get_many(keys)
+        all_docs_hash.values
+      end
+      
+      def all_keys
+        self.key_list_set.members
+      end
+      
+      def key_list_set 
+        @key_list_set || Riak::Crdt::Set.new(self.key_lists_bucket, self.key_list_set_name)
+      end
+      
+      def key_list_set_name
+        '_rg_keys_' + self.collection_name()
+      end
+      
+      # Return the bucket in which the Riagent collection key lists are kept
+      # @return [Riak::Bucket]
+      def key_lists_bucket
+        self.client.bucket('_rg_key_lists')
       end
     end
   end
--- test/unit/persistence_riak_dt_set_test.rb
+++ test/unit/persistence_riak_dt_set_test.rb
@@ -36,4 +36,43 @@ describe "a Riagent::ActiveDocument that persists via RiakDTSetStrategy" do
     Category.persistence.must_respond_to :all
     Category.persistence.must_respond_to :find
   end
+  
+  it "keeps collection key lists in a Set type bucket" do
+    Category.persistence.key_lists_bucket.must_be_kind_of Riak::Bucket
+    
+    Category.persistence.key_list_set.must_be_kind_of Riak::Crdt::Set
+    Category.persistence.key_list_set.key.must_equal '_rg_keys_categories'
+  end
+  
+  it "keeps a list of keys in a Crdt::Set object" do
+    Category.persistence.key_list_set = MiniTest::Mock.new
+    # Collection retrieves a list of all keys as a contents (members()) of a set object
+    Category.persistence.key_list_set.expect :members, []
+    Category.persistence.all_keys
+    Category.persistence.key_list_set.verify
+    
+    Category.persistence.key_list_set = nil  # reset
+  end
+  
+  it "uses a multi-get (get_many()) to fetch all documents in a collection" do
+    # Mock the key list operation
+    mock_key_list = ['123']
+    Category.persistence.key_list_set = MiniTest::Mock.new
+    Category.persistence.key_list_set.expect :members, mock_key_list  # return mock key list
+    
+    # Now mock and verify the 'get all documents' operation
+    mock_document = Category.new
+    mock_document.key = '123'
+    Category.persistence.bucket = MiniTest::Mock.new
+    
+    # get_many() returns the document key/value hash
+    Category.persistence.bucket.expect :get_many, { '123' => mock_document }, [mock_key_list]
+    all_docs = Category.all
+    Category.persistence.bucket.verify
+    all_docs.must_equal [ mock_document ]  # all() returns just the documents
+    
+    # Reset
+    Category.persistence.key_list_set = nil
+    Category.persistence.bucket = nil
+  end
 end
